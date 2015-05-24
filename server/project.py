bbd2865a8cc4643bf2e29e76e629bc7e3723fa8e
Ersel <ersel99@gmail.com>
2015-05-25 01:12:15 +0300

    added search area

--- server/project.py
+++ server/project.py
@@ -7,71 +7,123 @@ import codecs
 
 urls = (
     '/', 'index',
-    '/music', 'music',
+    '/music/(.*)', 'music',
     '/books', 'books',
-    '/book/(.*)', 'book'    
+    '/musics', 'musics',
+    '/book/(.*)', 'book',
+    '/about', 'about',
+    '/find_books', 'find_books',
+    '/find_musics', 'find_musics'    
 )
                
 render = web.template.render("templates", base="base")
 
-input_file  = file("../books.json", "r")
+book_file  = file("../books.json", "r")
 # data = json.loads(input_file.read().decode("utf-8-sig"))
-data = json.loads(input_file.read())
+book_data = json.loads(book_file.read())
+
+music_file  = file("../musics.json", "r")
+music_data = json.loads(music_file.read())
 
 
 #Utils
-def get_book_names(data):
-  book_names = []
-  for dicts in data:
-    book_names.append(dicts['name'])
-  return book_names
+def get_music_by_id(mid):
+  for m in music_data:
+    cover = m['cover']
+    music_id = cover.split('/')[-1]
+    if (music_id == mid):
+      music = {}
+      music['name'] = m['name']
+      music['composer'] = m['composer']
+      music['price'] = m['price']
+      music['content'] = m['content']
+      music['list_img'] = m['list_img']
+      music['img'] = cover
+      return music
 
 def get_book_by_id(bid):
-  for dicts in data:
-    cover = dicts['cover']
+  for b in book_data:
+    cover = b['cover']
     book_id = cover.split('/')[-1]
     if (book_id == bid):
       book = {}
-      book['name'] = dicts['name']
-      book['author'] = dicts['author']
-      book['price'] = dicts['price']
-      book['score'] = dicts['score']
-      book['content'] = dicts['content']
-      book['list_img'] = dicts['list_img']
+      book['name'] = b['name']
+      book['author'] = b['author']
+      book['price'] = b['price']
+      book['score'] = b['score']
+      book['content'] = b['content']
+      book['list_img'] = b['list_img']
+      book['reviews'] = b['reviews']
       book['img'] = cover
       return book
 
 
 class index:
     def GET(self):
+      return render.index()
+
+class musics:
+    def GET(self):
       ids_and_names = []
-      for element in data:
+      for element in music_data:
+        music = dict()
+        music['name'] = element['name']
+        cover = element['cover']
+        music['list_img'] = element['list_img']
+        music['id'] = cover.split('/')[-1]
+        music['composer'] = element['composer']
+        ids_and_names.append(music)
+      return render.musics(ids_and_names)
+       
+class music:
+    def GET(self, music_id):
+      
+      music = get_music_by_id(music_id)
+      from urllib import quote_plus
+      return render.music(music, quote_plus)
+
+class books:
+    def GET(self):
+      ids_and_names = []
+      for element in book_data:
         book = dict()
         book['name'] = element['name']
         cover = element['cover']
         book['list_img'] = element['list_img']
         book['id'] = cover.split('/')[-1]
+        book['author'] = element['author']
         ids_and_names.append(book)
-      return render.index(ids_and_names)
+      return render.books(ids_and_names)
 
-class music:
-    def GET(self):
-       return render.music()
-       
 class book:
     def GET(self, book_id):
       
       book = get_book_by_id(book_id)
       from urllib import quote_plus
       return render.book(book, quote_plus)
-
-class books:
+class about():
     def GET(self):
-      x = []
-      for i in data:
-        x.append(i['name'])
-      return json.dumps(x)
+        return render.about()
 
+class find_books():
+    def GET(self):
+      inp = web.input()
+      query_name = inp.get("query_name").lower()
+      found_books = []
+      for book in book_data:
+        if book["name"].lower().find(query_name) != -1:
+          found_books.append(book)
+      return json.dumps(found_books)
+
+class find_musics():
+    def GET(self):
+      inp = web.input()
+      query_name = inp.get("query_name").lower()
+      found_musics = []
+      for music in music_data:
+        if music["name"].lower().find(query_name) != -1:
+          found_musics.append(music)
+      return json.dumps(found_musics)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
---
+++ server/static/ajax.js
@@ -0,0 +1,107 @@
+/*
+$("input").on("keydown",function search(e) {
+    if(e.keyCode == 13) {
+        alert($(this).val());
+    }
+});
+*/
+$(document).ready(function (){
+
+	$("#book-search").on("keydown",function search(e) {
+    	if(e.keyCode == 13) {
+        	
+        	var query_name = $("#book-search").val()
+        	$.ajax({
+
+        		url:"find_books",
+        		data:{ 'query_name': query_name },
+        		dataType: "json",
+        		beforeSend: function() {
+        			console.log("before")
+        		},
+        		complete: function(data){
+        			console.log("completed")
+        			
+        		},
+        		success: function(data){
+        			//console.log(data)
+        			var bookResult = $("#bookResult");
+          			bookResult.empty();
+          			if (data.length == 0) {
+              			bookResult.html("Sorry, no book found.");
+            		}
+            		else{
+            			
+              			data.forEach(function(book) {
+                			var li = $("<li>");
+                			var img = $("<img>");
+                			var link = $("<a>")
+                			var name = book.name;
+                			var cover = book.cover;
+                			var splited = cover.split("/");
+                			var id = splited[splited.length-1];
+                			link.attr("href", "/book/" + id);
+                			img.attr("src",  book.list_img);
+                			link.append(img);
+                			link.append(name);
+                			li.html(book.author);
+                			li.prepend(link);          
+                			bookResult.append(li);          
+              			})
+              		
+            		}
+        		}
+        	});
+        	
+    	}
+	});
+	
+	$("#music-search").on("keydown",function search(e) {
+    	if(e.keyCode == 13) {
+        	
+        	var query_name = $("#music-search").val()
+        	$.ajax({
+
+        		url:"find_musics",
+        		data:{ 'query_name': query_name },
+        		dataType: "json",
+        		beforeSend: function() {
+        			console.log("before")
+        		},
+        		complete: function(data){
+        			console.log("completed")
+        			
+        		},
+        		success: function(data){
+        			console.log(data)
+        			var musicResult = $("#musicResult");
+          			musicResult.empty();
+          			if (data.length == 0) {
+              			musicResult.html("Sorry, no music found.");
+            		}
+            		else{
+            			
+              			data.forEach(function(music) {
+                			var li = $("<li>");
+                			var img = $("<img>");
+                			var link = $("<a>")
+                			var name = music.name;
+                			var cover = music.cover;
+                			var splited = cover.split("/");
+                			var id = splited[splited.length-1];
+                			link.attr("href", "/music/" + id);
+                			img.attr("src",  music.list_img);
+                			link.append(img);
+                			link.append(name);
+                			li.html(music.composer);
+                			li.prepend(link);          
+                			musicResult.append(li);          
+              			})
+              		
+            		}
+        		}
+        	});
+        	
+    	}
+	});
+});
